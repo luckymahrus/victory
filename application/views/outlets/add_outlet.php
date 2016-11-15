86b029c8f5f01454ae87cfbfabfc2c195f5483a9
felitas <felita.31895@gmail.com>
2016-11-15 16:12:16 +0700

    ?

--- application/views/outlets/add_outlet.php
+++ application/views/outlets/add_outlet.php
@@ -1,5 +1,5 @@
 <div class="row" style="padding-bottom: 10px;">	 
-	<h1 style="margin-bottom: 20px;">Tambah Outlet Baru</h1>
+	<a href="index.html" class="nav-button transform"><span></span></a><h1 style="margin-bottom: 20px;">Tambah Outlet Baru</h1>
     <hr class="bg-teal">	
 </div>
 <div class="row">
@@ -9,21 +9,31 @@
 				<label>Nama Toko</label>
                 <div class="input-control text full-size">
                     <input type="text" placeholder="Nama Toko">
+                    <button class="button helper-button clear"><span class="mif-cross"></span></button>
                 </div>
 			</div>
 			<div class="cell">
 				<label>Kode Toko</label>
                 <div class="input-control text full-size">
-                    <input type="text" placeholder="Masukkan Kode Toko">
-
+                    <input type="text" placeholder="Masukkan Kode Toko (Max. 2 Karakter)">
+                    <button class="button helper-button clear"><span class="mif-cross"></span></button>
                 </div>
 			</div>
 		</div>
 		<div class="row cells">
         	<div class="cell">
+        		<label>Nama Store Manager</label>
+        		<div class="input-control text full-size" data-role="input">
+				    <input type="text" placeholder="Nama Lengkap Store Manager">
+				    <button class="button helper-button clear"><span class="mif-cross"></span></button>
+				</div>
+        	</div>
+        </div>
+		<div class="row cells">
+        	<div class="cell">
         		<label>No. Telp</label>
         		<div class="input-control text full-size" data-role="input">
-				    <input type="text" placeholder="Nomor Telephone Sales">
+				    <input type="text" placeholder="Nomor Telephone Outlet">
 				    <button class="button helper-button clear"><span class="mif-cross"></span></button>
 				</div>
         	</div>
@@ -40,7 +50,7 @@
             <div class="cell">
                 <label>Username</label>
 				<div class="input-control text full-size" data-role="input">
-				    <input type="text" placeholder="Username Sales">
+				    <input type="text" placeholder="Username Outlet">
 				    <button class="button helper-button clear"><span class="mif-cross"></span></button>
 				</div>
             </div>
@@ -52,8 +62,17 @@
 				</div>
         	</div>
         </div>
+        <div class="row cells2">
+        	<div class="cell">
+        		<label>Margin Toko</label>
+				<div class="input-control text full-size" data-role="input">
+				    <input type="number" placeholder="Perbedaan Dasar Harga dengan Toko Utama">
+				    <button class="button" style="border-color: rgba(127, 140, 141,1.0); cursor: default;"><span class="fa fa-percent" aria-hidden="true"></span></button>  
+				</div>
+        	</div>
+        </div>
 		<div class="row text-center">
-        	<button class="button info bg-teal btn-teal">Submit</button>
+        	<input type="Submit" class="button info bg-teal btn-teal" value="Submit">
         </div>
 	</div>
 </div>
