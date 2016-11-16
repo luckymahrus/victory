8745fa13da885a195467f1235abfca3e245d7dd4
setyawan9 <setyawansusanto99@outlook.com>
2016-11-16 09:55:29 +0700

    Add Supplier fixed

--- application/views/supplier/add_supplier.php
+++ application/views/supplier/add_supplier.php
@@ -1,47 +1,53 @@
-<div class="row" style="padding-bottom: 10px;">	 
-	<a href="index.html" class="nav-button transform"><span></span></a><h1 style="margin-bottom: 20px;">Tambah Supplier Baru</h1>
-    <hr class="bg-teal">	
-</div>
-<div class="row">
-	<div class="grid">
-		<div class="row cells">
-			<div class="cell">
-				<label>Nama Supplier</label>
-                <div class="input-control text full-size">
-                    <input type="text" placeholder="Nama Supplier">
-                    <button class="button helper-button clear"><span class="mif-cross"></span></button>
-                </div>
-			</div>
-		</div>
-		<div class="row cells">
-        	<div class="cell">
-        		<label>No. Telp</label>
-        		<div class="input-control text full-size" data-role="input">
-				    <input type="text" placeholder="Nomor Telephone Supplier">
-				    <button class="button helper-button clear"><span class="mif-cross"></span></button>
-				</div>
-        	</div>
+<div class="grid">
+    <div class="row">
+        <div class="cell">
+            <h3><small><a href=""><span class="fa fa-arrow-circle-o-left"></span> Kembali ke Daftar Supplier</a></small></h3>
         </div>
-        <div class="row cells">
-            <div class="cell">
-                <label>E-mail</label>
-                <div class="input-control text full-size" data-role="input">
-                    <input type="text" placeholder="Email Supplier">
-                    <button class="button helper-button clear"><span class="mif-cross"></span></button>
-                </div>
+    </div>
+    <div class="row">
+        <div class="cell">
+            <h1 style="margin-bottom: 20px;">Tambah Supplier Baru</h1>
+            <hr class="bg-teal">    
+        </div>
+    </div>
+    <div class="row">
+        <div class="cell">
+            <label>Nama Supplier</label>
+            <div class="input-control text full-size">
+                <input type="text" placeholder="Nama Supplier">
+                <button class="button helper-button clear"><span class="mif-cross"></span></button>
             </div>
         </div>
-        <div class="row cells">
-        	<div class="cell">
-        		<label>Alamat</label>
-        		<div class="input-control textarea full-size" data-role="input" data-text-auto-resize="true">
-				    <textarea></textarea>
-				</div>
-        	</div>
+    </div>
+    <div class="row">
+        <div class="cell">
+            <label>No. Telp</label>
+            <div class="input-control text full-size">
+                <input type="text" placeholder="Nomor Telephone Supplier">
+                <button class="button helper-button clear"><span class="mif-cross"></span></button>
+            </div>
         </div>
-        
-		<div class="row text-center">
-        	<input type="Submit" class="button info bg-teal btn-teal" value="Submit">
+    </div>
+    <div class="row">
+        <div class="cell">
+            <label>Email</label>
+            <div class="input-control text full-size">
+                <input type="text" placeholder="Email Supplier">
+                <button class="button helper-button clear"><span class="mif-cross"></span></button>
+            </div>
         </div>
-	</div>
+    </div>
+    <div class="row">
+        <div class="cell">
+            <label>Alamat</label>
+            <div class="input-control textarea full-size" data-role="input" data-text-auto-resize="true">
+                <textarea></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="cell">
+            <input type="Submit" class="button info bg-teal btn-teal" value="Submit">
+        </div>
+    </div>
 </div>
