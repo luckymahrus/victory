6177d291f4834e8dc14c464c8cfd770822d1e2e4
whadever <irpanwinata@gmail.com>
2016-11-18 10:03:19 +0700

    crud model

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -2,23 +2,23 @@
 	
 class Crud_model extends CI_Model{
 
-	public function insert_data($table,$data){
+	function insert_data($table,$data){
 		$this->db->insert($table,$data);
 	}
 
-	public function update_data($table,$data,$condition){
+	function update_data($table,$data,$condition){
 		$this->db->update($table,$data,$condition);
 	}
 
-	public function get_data($table){
+	function get_data($table){
 		return $this->db->get($table);
 	}
 
-	public function get_by_condition($table,$condition){
+	function get_by_condition($table,$condition){
 		return $this->db->get_where($table,$condition);
 	}
 
-	public function delete_data($table,$condition){
+	function delete_data($table,$condition){
 		$this->db->delete($table,$condition);
 	}
 
